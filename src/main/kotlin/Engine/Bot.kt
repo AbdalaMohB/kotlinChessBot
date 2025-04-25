1463a29b745e8cbdf7fcb1273595a10bf47d4eec
AbdalaMohB <120782158+AbdalaMohB@users.noreply.github.com>
2025-04-25 22:17:06 +0300

    Update Bot.kt

--- src/main/kotlin/Engine/Bot.kt
+++ src/main/kotlin/Engine/Bot.kt
@@ -4,6 +4,13 @@ import kotlin.random.Random
 import kotlin.system.exitProcess
 
 class Bot(var root:Node, private val maxdepth:Int=4) {
+//    public fun evaluate(){
+//
+//    }
+private var isEnd: Int = when((maxdepth-1)%2){
+        0 -> maxdepth-1
+        else -> maxdepth-2
+    }
 
     public fun choose() : MutableList<MutableList<Int>> {
         val maxval=root.next.maxBy { it.value }.value
@@ -45,6 +52,7 @@ class Bot(var root:Node, private val maxdepth:Int=4) {
                 node.next.addLast(n)
             }
         }
+        //evaluateNode(node)
     }
     public fun counter(node: Node=root, depth: Int=1){
         applier(node, -1){ moves, y, x ->
@@ -57,16 +65,21 @@ class Bot(var root:Node, private val maxdepth:Int=4) {
                 node.next.addLast(n)
             }
         }
-        evaluateNode(node)
-        val lowval=node.next.minBy { it.value }.value
-        val pruned=node.next.filter { it.value==lowval }
-        
-        node.next=pruned.toMutableList()
+
+        if (depth>2) {
+            evaluateNode(node)
+            val lowval = node.next.maxBy { it.value }.value
+            val pruned = node.next.filter { it.value < lowval }
+            //val rng=abs(Random.nextInt())%pruned.size
+            node.next = pruned.toMutableList()
+        }
         for (n in node.next){
             ponder(n, depth+1)
         }
     }
     public fun makeMove(board:MutableList<MutableList<Int>>, y:Int, x:Int, pair:Pair<Int, Int>): MutableList<MutableList<Int>>{
+        //if (board[y][x]<8) board[y][x]+=1
+        //else if (board[y][x]>-8) board[y][x]-=1
         if (board[y][x]==1 && pair.first==7){
             board[pair.first][pair.second] = 15
             board[y][x] = 0
